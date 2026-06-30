5b61280eb0378ad5ec73ab04d190708045171c91
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-06-30 16:34:06 +0100

    Rate Limiting 3

--- src/main/java/com/example/api_gateway/config/RateLimiterConfiguration.java
+++ src/main/java/com/example/api_gateway/config/RateLimiterConfiguration.java
@@ -6,75 +6,51 @@ import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@RequiredArgsConstructor
-public class RateLimiterConfiguration {
-
-    private final RateLimitProperties properties;
-
-    @Bean
-    public RedisRateLimiter searchRateLimiter() {
-
-        return new RedisRateLimiter(
-
-                properties.getSearch().getReplenishRate(),
-
-                properties.getSearch().getBurstCapacity()
-
-        );
-
-    }
-
-    @Bean
-    public RedisRateLimiter uploadRateLimiter() {
-
-        return new RedisRateLimiter(
-
-                properties.getUpload().getReplenishRate(),
-
-                properties.getUpload().getBurstCapacity()
-
-        );
-
-    }
-
-    @Bean
-    public RedisRateLimiter downloadRateLimiter() {
-
-        return new RedisRateLimiter(
-
-                properties.getDownload().getReplenishRate(),
-
-                properties.getDownload().getBurstCapacity()
-
-        );
-
-    }
-
-    @Bean
-    public RedisRateLimiter paperRateLimiter() {
-
-        return new RedisRateLimiter(
-
-                properties.getPaper().getReplenishRate(),
-
-                properties.getPaper().getBurstCapacity()
-
-        );
-
-    }
-
-    @Bean
-    public RedisRateLimiter categoryRateLimiter() {
-
-        return new RedisRateLimiter(
-
-                properties.getCategory().getReplenishRate(),
-
-                properties.getCategory().getBurstCapacity()
-
-        );
-
-    }
-
-}
+//@Configuration
+//@RequiredArgsConstructor
+//public class RateLimiterConfiguration {
+//
+//    private final RateLimitProperties properties;
+//
+//    @Bean
+//    public RedisRateLimiter searchRateLimiter() {
+//        return new RedisRateLimiter(
+//                properties.getSearch().getReplenishRate(),
+//                properties.getSearch().getBurstCapacity()
+//        );
+//    }
+//
+//    @Bean
+//    public RedisRateLimiter uploadRateLimiter() {
+//        return new RedisRateLimiter(
+//                properties.getUpload().getReplenishRate(),
+//                properties.getUpload().getBurstCapacity()
+//        );
+//    }
+//
+//    @Bean
+//    public RedisRateLimiter downloadRateLimiter() {
+//        return new RedisRateLimiter(
+//                properties.getDownload().getReplenishRate(),
+//                properties.getDownload().getBurstCapacity()
+//        );
+//    }
+//
+//    @Bean
+//    public RedisRateLimiter paperRateLimiter() {
+//        return new RedisRateLimiter(
+//                properties.getPaper().getReplenishRate(),
+//                properties.getPaper().getBurstCapacity()
+//        );
+//    }
+//
+//    @Bean
+//    public RedisRateLimiter categoryRateLimiter() {
+//        return new RedisRateLimiter(
+//                properties.getCategory().getReplenishRate(),
+//                properties.getCategory().getBurstCapacity()
+//
+//        );
+//    }
+//
+//}
--- src/main/java/com/example/api_gateway/util/RateLimitProperties.java
+++ src/main/java/com/example/api_gateway/util/RateLimitProperties.java
@@ -3,22 +3,16 @@ package com.example.api_gateway.util;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ConfigurationProperties(prefix = "gateway.rate-limit")
 @Getter
 @Setter
+@ConfigurationProperties(prefix = "gateway.rate-limit")
 public class RateLimitProperties {
 
     private Endpoint search = new Endpoint();
-
     private Endpoint upload = new Endpoint();
-
     private Endpoint download = new Endpoint();
-
     private Endpoint paper = new Endpoint();
-
     private Endpoint category = new Endpoint();
 
     @Getter
@@ -26,7 +20,6 @@ public class RateLimitProperties {
     public static class Endpoint {
 
         private int replenishRate;
-
         private int burstCapacity;
 
     }
